17c865361cfa6b6dd6ff01d2b604a2fc3ac08915
yitzy299 <yitzy299@noreply.github.com>
2021-07-30 19:18:06 +0100

    Rewrite in Kotlin
    They said it was impossible
    
    (Excluding the mixins and duck interface)

---
+++ src/main/kotlin/com/github/quiltservertools/interdimensional/command/argument/DimensionOverrideArgumentType.kt
@@ -0,0 +1,18 @@
+package com.github.quiltservertools.interdimensional.command.argument
+
+import com.mojang.brigadier.arguments.BoolArgumentType
+import com.mojang.brigadier.arguments.LongArgumentType
+import com.mojang.brigadier.arguments.StringArgumentType
+import net.minecraft.command.argument.DimensionArgumentType
+
+object DimensionOverrideArgumentType : AbstractInterdimensionalArgumentType() {
+
+    init {
+        criteriumSuggestors["seed"] = Suggestor(LongArgumentType.longArg())
+        criteriumSuggestors["type"] = Suggestor(DimensionArgumentType.dimension())
+        criteriumSuggestors["generator"] = Suggestor(DimensionArgumentType.dimension())
+        criteriumSuggestors["difficulty"] = Suggestor(StringArgumentType.string())
+        criteriumSuggestors["custom_generator"] = Suggestor(BoolArgumentType.bool())
+        criteria = criteriumSuggestors.keys
+    }
+}
